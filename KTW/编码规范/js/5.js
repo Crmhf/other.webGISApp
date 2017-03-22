b979841eab5935ebaa0542fdd45bc9088eee512f
Crmhf <hci.crmhf@gmail.com>
2017-03-22 14:00:51 +0800

    add demo.

---
+++ KTW/编码规范/js/5.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright © 2017 KingTopWare Corporation. All rights reserved.
+ *
+ * @title: 5
+ * @prject: WebStorm
+ * @description: 说明文件的功能--请后续修改
+ * @author: ChiRong
+ * @date: 2017/3/21
+ * @version:
+ * @modify: 说明对该文件的修改内容、修改原因以及修改日期--请后续修改
+ */
