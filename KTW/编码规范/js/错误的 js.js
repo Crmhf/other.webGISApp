a1fa150e7393cc4e3eb6efdfce8cd807421af2a6
crmhf <hci.crmhf@gmail.com>
2017-04-12 14:58:56 +0800

    add 前端编码试题方面的内容

---
+++ KTW/编码规范/js/错误的 js.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright © 2017 KingTopWare Corporation. All rights reserved.
+ *
+ * @title: 错误的 js
+ * @prject:
+ * @description: 说明文件的功能--请后续修改
+ * @author: ChiRong
+ * @date: 2017/4/12
+ * @version: v1.0
+ * @modify: 说明对该文件的修改内容、修改原因以及修改日期--请后续修改
+ */
+
+
+
+function(res) {
+	var code = 1 + 1, json = JSON.parse(res), type, html;
+	var KTW-location = "http://192.168.1.111/temp/index.html";
+}
+var a,b = 1;
+var name = 'ktw';
+a=b
+(function(){
+	a =+1;
+	if (true)
+	alert(name);
+})()
