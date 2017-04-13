c29145e15f95c6c042bfa389e783b8bf5f4295ef
crmhf <hci.crmhf@gmail.com>
2017-04-13 15:39:35 +0800

    add demo.

---
+++ KTW/WebStorm模板/js/个人.通用模板.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright © 2017 KingTopWare Corporation. All rights reserved.
+ *
+ * @title: 个人.通用模板
+ * @prject:
+ * @description: 说明文件的功能--请后续修改
+ * @author: ChiRong
+ * @date: 2017/4/12
+ * @version: v1.0
+ * @modify: 说明对该文件的修改内容、修改原因以及修改日期--请后续修改
+ */
+
+/**
+ * 函数描述
+ *
+ * @param {string} p1 参数1的说明
+ * @param {string} p2 参数2的说明，比较长
+ *     那就换行了.
+ * @param {number=} p3 参数3的说明（可选）
+ * @return {Object} 返回值描述
+ */
+function foo(p1, p2, p3){
+	var p3 = p3 || 10;
+	return {
+		p1 : p1,
+		p2 : p2,
+		p3 : p3
+	};
+}
